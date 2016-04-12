d39b1525ef410f7599330301d88ab39d0a55a32a
casperlamboo <casperlamboo@gmail.com>
2016-04-12 12:51:37 +0200

    added optional offset and scale arguments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import Shape from 'clipper-js';
 
-export default function(paths, point, offset = 1.0) {
+export default function(paths, point, { offset = 1.0, scale = 1.0 } = {}) {
+  offset *= scale;
   // convert point to uppercase
-  point = { X: point.x, Y: point.y };
+  point = { X: point.x * scale, Y: point.y * scale };
   // instanciate new shape
   const shape = new Shape(paths, false, true);
 
   const shapes = shape
+    // scale up for precision
+    .scaleUp(scale)
     // convert lines to polygons (this gives lines width)
     .offset(offset, { joinType: 'jtSquare', endType: 'etOpenButt' })
     // union all overlapping paths
@@ -20,7 +23,9 @@ export default function(paths, point, offset = 1.0) {
     if (fill.pointInShape(point)) { // check if point has hit with shape
       return fill // return fill
         // offset result to account for converting to polygon
-        // .offset(offset, { joinType: 'jtSquare', endType: 'etClosedPolygon' })
+        .offset(offset, { joinType: 'jtSquare', endType: 'etClosedPolygon' })
+        // scale down to accout for scale up
+        .scaleDown(scale)
         // convert uppercase points to lowercase points
         .mapToLower();
     }
