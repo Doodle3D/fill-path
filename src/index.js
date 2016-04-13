456617dab79960d85fe525109ed24b742c59397f
casperlamboo <casperlamboo@gmail.com>
2016-04-13 10:06:46 +0200

    added miter limit argument

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Shape from 'clipper-js';
 
-export default function(paths, point, { offset = 1.0, scale = 1.0 } = {}) {
+export default function(paths, point, { offset = 1.0, scale = 1.0, miterLimit = 2.0 } = {}) {
   offset *= scale;
   // convert point to uppercase
   point = { X: point.x * scale, Y: point.y * scale };
@@ -11,7 +11,7 @@ export default function(paths, point, { offset = 1.0, scale = 1.0 } = {}) {
     // scale up for precision
     .scaleUp(scale)
     // convert lines to polygons (this gives lines width)
-    .offset(offset, { joinType: 'jtSquare', endType: 'etOpenButt' })
+    .offset(offset, { jointType: 'jtMiter', endType: 'etOpenButt', miterLimit })
     // union all overlapping paths
     .removeOverlap()
     // make all holes outlines and all outlines holes
@@ -23,7 +23,7 @@ export default function(paths, point, { offset = 1.0, scale = 1.0 } = {}) {
     if (fill.pointInShape(point)) { // check if point has hit with shape
       return fill // return fill
         // offset result to account for converting to polygon
-        .offset(offset, { joinType: 'jtSquare', endType: 'etClosedPolygon' })
+        .offset(offset, { jointType: 'jtMiter', endType: 'etClosedPolygon', miterLimit })
         // scale down to accout for scale up
         .scaleDown(scale)
         // convert uppercase points to lowercase points
