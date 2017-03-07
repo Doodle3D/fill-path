69af964c7d8eece7dcc57894a23e67b7276cbd51
casperlamboo <casperlamboo@gmail.com>
2017-03-08 00:27:06 +0100

    more logical structure

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ export default function(paths, point, {
   miterLimit = 2.0,
   fillOffset = CENTER
 } = {}) {
+  // calculate linewith
   lineWidth *= scale / 2.0;
   // convert point to uppercase
   point = { X: point.x * scale, Y: point.y * scale };
-  // instanciate new shape
-  const shape = new Shape(paths, false, true);
 
-  const shapes = shape
+  // create clipper shape
+  const shape = new Shape(paths, false, true, true, true)
     // scale up for precision
     .scaleUp(scale)
     // convert lines to polygons (this gives lines width)
@@ -26,8 +26,14 @@ export default function(paths, point, {
     // make all holes outlines and all outlines holes
     .reverse()
     // seperate all shapes into (these all all plausible fills)
-    .seperateShapes();
+    .seperateShapes()
+    // find shape with hit
+    .find(fill => fill.pointInShape(point));
 
+  // if no fill has been found return empty shape
+  if (!shape) return [];
+
+  // determine what offset should be used when returning shape
   let offset;
   switch (fillOffset) {
     case CENTER:
@@ -44,19 +50,11 @@ export default function(paths, point, {
       break;
   }
 
-  for (const fill of shapes) { // loop trough all
-    if (fill.pointInShape(point)) { // check if point has hit with shape
-
-      return fill // return fill
-        // offset result to account for converting to polygon
-        .offset(offset, { jointType: 'jtMiter', endType: 'etClosedPolygon', miterLimit })
-        // scale down to accout for scale up
-        .scaleDown(scale)
-        // convert uppercase points to lowercase points
-        .mapToLower();
-    }
-  }
-
-  // if no fill has been found return empty shape
-  return [];
+  return shape
+    // offset result to account for converting to polygon
+    .offset(offset, { jointType: 'jtMiter', endType: 'etClosedPolygon', miterLimit })
+    // scale down to accout for scale up
+    .scaleDown(scale)
+    // convert uppercase points to lowercase points
+    .mapToLower();
 }
